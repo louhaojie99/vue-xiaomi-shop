50a1f4ff7d8f711fb21dd893767a35bf446d6f38
louhaojie99 <851681631@qq.com>
2020-09-28 20:19:10 +0800

    hj12

--- src/views/home/search.vue
+++ src/views/home/search.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="">
-    11111111
+  <div class="search">
+    <div>
+      <van-icon class="i" name="arrow-left" />
+    </div>
+    <div>
+      <input class="serach" type="search" placeholder="搜索商品名称" />
+    </div>
+    <div>
+      <van-icon class="i" name="search" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: "search",
   data() {
     return {};
   },
@@ -17,4 +25,42 @@ export default {
   methods: {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search {
+  height: 1.106667rem;
+  background-color: #f2f2f2;
+  display: flex;
+  div {
+    height: 1.1rem;
+  }
+  :nth-of-type(1) {
+    width: 1.2rem;
+    .i {
+      font-size: 0.8rem;
+      color: #9c9c9c;
+      text-align: center;
+      line-height: 1.1rem;
+    }
+  }
+  :nth-of-type(2) {
+    flex: 1;
+    .serach {
+      width: 6.9664rem;
+      height: 0.9072rem;
+      margin-top: 0.1rem;
+      margin-left: 0.4rem;
+      border: none;
+      text-indent: 0.3rem;
+    }
+  }
+  :nth-of-type(3) {
+    width: 1.3rem;
+    .i {
+      color: #9c9c9c;
+      font-size: 0.8rem;
+      text-align: center;
+      line-height: 1.1rem;
+    }
+  }
+}
+</style>
