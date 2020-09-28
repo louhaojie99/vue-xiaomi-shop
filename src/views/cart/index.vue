83023f6e5db59097fbce8199be427e88f140b59d
louhaojie99 <851681631@qq.com>
2020-09-28 16:07:20 +0800

    hj05

--- src/views/cart/index.vue
+++ src/views/cart/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="">
-    购物车
-
+    <!--
+      搜索栏
+          参数： fixed固定在顶部
+                placeholder 固定在顶部时，是否在标签位置生成一个等高的占位元素
+    -->
+    <van-nav-bar
+      title="购物车"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickSearch"
+    >
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
     <!-- 页脚 -->
     <FooterBar></FooterBar>
   </div>
@@ -21,7 +35,19 @@ export default {
   },
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onClickLeft() {
+      console.log("返回");
+    },
+    onClickSearch() {
+      console.log("搜索");
+    },
+  },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.red {
+  color: red;
+  background: red;
+}
+</style>
