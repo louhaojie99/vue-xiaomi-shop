0e91ad75c8eace3946ccb6b8f12ffbeb2422ef0f
louhaojie99 <851681631@qq.com>
2020-09-28 16:42:01 +0800

    hj06

--- src/views/cart/index.vue
+++ src/views/cart/index.vue
@@ -11,11 +11,27 @@
       left-arrow
       @click-left="onClickLeft"
       @click-right="onClickSearch"
+      class="cartnav"
     >
-      <template #right>
-        <van-icon name="search" size="18" />
+      <template #right class="bbb">
+        <van-icon class="bbb" name="search" size="18" />
       </template>
     </van-nav-bar>
+    <div class="cartCon">
+      <van-cell
+        class="msgLogin"
+        title="登陆后享受更多优惠"
+        is-link
+        value="去登陆"
+        click="goLogin"
+      />
+      <div class="cart-bom">
+        <van-icon class="i" name="cart-o" />
+        <span class="span">购物车还是空的</span>
+        <em class="em" @click="goProList">去逛逛</em>
+      </div>
+    </div>
+
     <!-- 页脚 -->
     <FooterBar></FooterBar>
   </div>
@@ -42,12 +58,50 @@ export default {
     onClickSearch() {
       console.log("搜索");
     },
+    goLogin() {
+      console.log(1);
+    },
+    // 点击去逛逛跳转到首页
+    goProList() {
+      this.$router.push({
+        path: "/home",
+      });
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
-.red {
-  color: red;
-  background: red;
+.cartnav {
+  background: #f1f1f1;
+}
+.bbb {
+  // color: #ccc;
+  font-weight: bold;
+}
+.cartCon {
+  .msgLogin {
+    background: #ffffff;
+    border-bottom: 0.0133rem solid #cccccc;
+  }
+  .cart-bom {
+    height: 1.666667rem;
+    background-color: #f1f1f1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .i {
+      font-size: 1rem;
+    }
+    .span {
+      font-size: 0.426667rem;
+      margin: 0 0.133333rem;
+    }
+    .em {
+      display: block;
+      padding: 0.0667rem 0.3333rem;
+      border: 1px solid #ccc;
+      font-style: normal;
+    }
+  }
 }
 </style>
